966c5ec9e8aa2a16020446281fbf67bf7ca865e2
Crawl <icrawltogo@gmail.com>
2016-10-19 07:05:02 +0200

    Dump

---
+++ Commando.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+const commando = require('discord.js-commando');
+const path = require('path');
+const oneLine = require('common-tags').oneLine;
+const token = require('./auth.json').token;
+
+const client = new commando.Client({
+	owner: '81440962496172032',
+	commandPrefix: '>',
+	unknownCommandResponse: false
+});
+
+client.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	.on('ready', () => {
+		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
+	.on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnect', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if (err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+	})
+	.on('commandPrefixChange', (guild, prefix) => {
+		console.log(oneLine`
+			Prefix changed to ${prefix || 'the default'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('commandStatusChange', (guild, command, enabled) => {
+		console.log(oneLine`
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('groupStatusChange', (guild, group, enabled) => {
+		console.log(oneLine`
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	});
+
+client.registry
+	.registerDefaults()
+	.registerGroups([
+		['info', 'Info']
+	])
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.login(token);
---
+++ commands/info/about.js
@@ -0,0 +1,37 @@
+const stripIndents = require('common-tags').stripIndents;
+const { Command } = require('discord.js-commando');
+
+module.exports = class AboutCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'about',
+			group: 'info',
+			memberName: 'about',
+			description: 'Displays information about the command framework.'
+		});
+	}
+
+	async run(msg) {
+		msg.say(stripIndents`
+				__**discord.js Commando:**__
+				This is the WIP official command framework for discord.js' indev branch.
+				It makes full use of ES2017's \`async\`/\`await\`.
+
+				GitHub: <https://github.com/Gawdl3y/discord.js-commando>
+
+				__**Installation:**__
+				**Node 7.0.0 or newer is required.** Use a nightly or beta version.
+				\`npm install --save discord.js-commando\`
+
+				When running a bot using Commando, make sure to run Node with the \`--harmony\` flag.
+				Example:
+				\`node --harmony somebot.js\`
+				\`pm2 start somebot.js --node-args='--harmony'\`
+
+				__**Documentation (WIP):**__
+				<https://gawdl3y.github.io/discord.js-commando/0.1.0/>
+				See also:
+				<http://hydrabolt.github.io/discord.js/#!/docs/tag/indev/file/general/Welcome>
+		`);
+	}
+};
