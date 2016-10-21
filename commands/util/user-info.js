f26e2616c70db0ba4d5cd2d7e5aa3e57984162d1
Crawl <icrawltogo@gmail.com>
2016-10-21 08:38:44 +0200

    Whoopiedo

---
+++ commands/util/user-info.js
@@ -0,0 +1,46 @@
+const moment = require('moment');
+const stripIndents = require('common-tags').stripIndents;
+const { Command } = require('discord.js-commando');
+
+module.exports = class UserInfoCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'user-info',
+			aliases: ['user', '🗒'],
+			group: 'util',
+			memberName: 'user-info',
+			description: 'Get info on a user.',
+			useage: '<user>',
+			details: `Get detailed information on the specified user.`,
+			examples: ['userinfo @Crawl#3208', 'user Crawl'],
+			guildOnly: true,
+
+			args: [
+				{
+					key: 'member',
+					prompt: 'What user would you like information on?\n',
+					type: 'member'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const member = args.member;
+		const user = member.user;
+
+		return msg.say(stripIndents`
+					Info on **${user.username}#${user.discriminator}** (ID: ${user.id})
+
+					**❯ Member Details**
+					${member.nickname !== null ? ` • Nickname: ${member.nickname}` : ' • No nickname'}
+					 • Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(' ')}
+					 • Joined at: ${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}
+
+					**❯ User Details**
+					 • Created at: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}${user.bot ? '\n • Is a bot account' : ''}
+					 • Status: ${user.presence.status}
+					 • Game: ${user.presence.game ? user.presence.game.name : 'None'}
+		`);
+	}
+};
