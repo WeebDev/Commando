bfaf833a69fc87757fa66c56fff0ce7d98fb307c
Crawl <icrawltogo@gmail.com>
2016-11-02 09:00:49 +0100

    docs

---
+++ docs/types/DocumentedVarType.js
@@ -0,0 +1,46 @@
+const DocumentedItem = require('./DocumentedItem');
+
+/*
+{
+  "names":[
+    "String"
+  ]
+}
+*/
+
+const regex = /([\w]+)([^\w]+)/;
+const regexG = /([\w]+)([^\w]+)/g;
+
+function splitVarName(str) {
+	if (str === '*') {
+		return ['*', ''];
+	}
+	const matches = str.match(regexG);
+	const output = [];
+	if (matches) {
+		for (const match of matches) {
+			const groups = match.match(regex);
+			output.push([groups[1], groups[2]]);
+		}
+	} else {
+		output.push([str.match(/(\w+)/g)[0], '']);
+	}
+	return output;
+}
+
+module.exports = class DocumentedVarType extends DocumentedItem {
+
+	registerMetaInfo(data) {
+		super.registerMetaInfo(data);
+		this.directData = data;
+	}
+
+	serialize() {
+		super.serialize();
+		const names = [];
+		for (const name of this.directData.names) {
+			names.push(splitVarName(name));
+		}
+		return { types: names };
+	}
+};
