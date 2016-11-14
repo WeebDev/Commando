ab2f72ee94f428573d7dd70544607b5a19357c26
Crawl <icrawltogo@gmail.com>
2016-11-14 02:43:07 +0100

    What about 50?

--- commands/fun/anime.js
+++ commands/fun/anime.js
@@ -47,7 +47,7 @@ module.exports = class AnimeCommand extends Command {
 			}
 			data = data.length === 1 ? data[0] : data.find(en => en.title_english.toLowerCase() === anime.toLowerCase() || en.title_romaji.toLowerCase() === anime.toLowerCase()) || data[0];
 			let title = data.title_english !== '' && data.title_romaji !== data.title_english ? `${data.title_english} / ${data.title_romaji} / ${data.title_japanese}` : `${data.title_romaji} / ${data.title_japanese}`;
-			let synopsis = data.description.replace(/\\n/g, '\n').replace(/<br>|\\r/g, '').substring(0, 1100);
+			let synopsis = data.description.replace(/\\n/g, '\n').replace(/<br>|\\r/g, '').substring(0, 1050);
 			let score = data.average_score / 10;
 
 			// It would be horrible if she wouldn't stop typing
