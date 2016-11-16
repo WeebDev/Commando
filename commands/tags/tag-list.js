562dfb7cde115e93069cf43df56aa2debd35d607
Crawl <icrawltogo@gmail.com>
2016-11-16 11:07:45 +0100

    Images and Thumbnails finally!

--- commands/tags/tag-list.js
+++ commands/tags/tag-list.js
@@ -19,9 +19,24 @@ module.exports = class TagListCommand extends Command {
 	async run(msg) {
 		return TagModel.find(msg.guild.id).then(tags => {
 			if (!tags) return msg.say(`${msg.guild.name} doesn't have any tags, ${msg.author}. Why not add one?`);
-			return msg.say(stripIndents`**❯ Tags:**
-				${tags.map(tag => tag.name).join(', ')}
-			`);
+			let tagListMessage = {
+				color: 3447003,
+				author: {
+					name: `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`,
+					icon_url: `${msg.author.avatarURL}` // eslint-disable-line camelcase
+				},
+				description: stripIndents`
+					**❯ Tags:**
+					${tags.map(tag => tag.name).sort().join(', ')}
+				`,
+				timestamp: new Date(),
+				footer: {
+					icon_url: this.client.user.avatarURL, // eslint-disable-line camelcase
+					text: 'Tag list'
+				}
+			};
+
+			return msg.channel.sendMessage('', { embed: tagListMessage });
 		}).catch(error => { winston.error(error); });
 	}
 };
