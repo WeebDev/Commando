ffae2508f863b82b7a280fe37a88efee4be9c44f
Crawl <icrawltogo@gmail.com>
2016-11-30 22:18:00 +0900

    Fix up soundcloud, change some tags stuff

--- commands/tags/tag-list.js
+++ commands/tags/tag-list.js
@@ -21,7 +21,19 @@ module.exports = class TagListCommand extends Command {
 
 	async run(msg) {
 		let tags = await Tag.findAll({ where: { guildID: msg.guild.id } });
-		if (!tags) return msg.say(`${msg.guild.name} doesn't have any tags, ${msg.author}. Why not add one?`);
+		if (!!tags) return msg.say(`${msg.guild.name} doesn't have any tags, ${msg.author}. Why not add one?`); // eslint-disable-line no-extra-boolean-cast
+
+		let examples = tags.filter(tag => tag.type)
+			.filter(tag => tag.example)
+			.map(tag => tag.name)
+			.sort()
+			.join(', ');
+
+		let usertags = tags.filter(tag => !tag.type)
+			.filter(tag => tag.userID === msg.author.id)
+			.map(tag => tag.name)
+			.sort()
+			.join(', ');
 
 		return msg.say(stripIndents`**❯ Tags:**
 			${tags.filter(tag => tag.type)
@@ -30,19 +42,11 @@ module.exports = class TagListCommand extends Command {
 				.sort()
 				.join(', ')}
 
-			**❯ Examples:**
-			${tags.filter(tag => tag.type)
-				.filter(tag => tag.example)
-				.map(tag => tag.name)
-				.sort()
-				.join(', ')}
+			${examples ? `**❯ Examples:**
+				examples` : `There are no examples.`}
 
-			**❯ ${msg.author.username}'s tags:**
-			${tags.filter(tag => !tag.type)
-				.filter(tag => tag.userID === msg.author.id)
-				.map(tag => tag.name)
-				.sort()
-				.join(', ')}
+			${usertags ? `**❯ ${msg.author.username}'s tags:**
+				usertags` : `${msg.author.username} has no tags.`}
 		`);
 	}
 };
