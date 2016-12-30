7fb7a9d5256cff7d8c4a119d6a58f6da4dfc27c1
vzwGrey <thegreylp@googlemail.com>
2016-12-30 12:04:12 +0100

    Change payout

--- Commando.js
+++ Commando.js
@@ -55,7 +55,7 @@ client.on('error', winston.error)
 		if (earnedRecently.includes(message.author.id)) return;
 
 		const hasImageAttachment = message.attachments.some(attachment => attachment.url.match(/\.(png|jpg|jpeg|gif|webp)$/));
-		const moneyEarned = hasImageAttachment ? 40 : 5;
+		const moneyEarned = hasImageAttachment ? 5 : 2;
 
 		currency.addBalance(message.author.id, moneyEarned);
 
