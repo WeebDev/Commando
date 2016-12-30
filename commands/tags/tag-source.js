f1231118f567dc5c405cd4a106ad024db01571a4
Crawl <icrawltogo@gmail.com>
2016-12-30 11:21:26 +0900

    Add tag-source

---
+++ commands/tags/tag-source.js
@@ -0,0 +1,58 @@
+const { Command } = require('discord.js-commando');
+
+const Redis = require('../../redis/Redis');
+const Tag = require('../../postgreSQL/models/Tag');
+
+const redis = new Redis();
+
+module.exports = class TagSourceCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'tag',
+			group: 'tags',
+			memberName: 'tag',
+			description: 'Displays a tag.',
+			format: '<tagname>',
+			guildOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 3
+			},
+
+			args: [
+				{
+					key: 'name',
+					label: 'tagname',
+					prompt: 'What tag-source would you like to see?\n',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const name = args.name.toLowerCase();
+
+		return this.findTagCached(msg, name, msg.guild.id);
+	}
+
+	async findTagCached(msg, name, guildID) {
+		return redis.db.getAsync(name + guildID).then(async reply => {
+			if (reply) {
+				let tag = await Tag.findOne({ where: { name: name, guildID: guildID } });
+				if (tag) tag.increment('uses');
+
+				return msg.code('', reply);
+			} else {
+				let tag = await Tag.findOne({ where: { name: name, guildID: guildID } });
+				if (!tag) return msg.say(`A tag with the name **${name}** doesn't exist, ${msg.author}`);
+				tag.increment('uses');
+
+				return redis.db.setAsync(name + guildID, tag.content)
+					.then(() => {
+						msg.code('', tag.content);
+					});
+			}
+		});
+	}
+};
