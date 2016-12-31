40b6fac53c38c835b036ce340b53e51bd8a85552
Crawl <icrawltogo@gmail.com>
2016-12-31 19:10:57 +0900

    Cache leaderboard

--- commands/currency/money-leaderboard.js
+++ commands/currency/money-leaderboard.js
@@ -3,6 +3,9 @@ const stripIndents = require('common-tags').stripIndents;
 
 const config = require('../../settings');
 const Money = require('../../postgreSQL/models/Money');
+const Redis = require('../../redis/Redis');
+
+const redis = new Redis();
 
 module.exports = class MoneyLeaderboardCommand extends Command {
 	constructor(client) {
@@ -28,21 +31,37 @@ module.exports = class MoneyLeaderboardCommand extends Command {
 
 	async run(msg, args) {
 		const page = args.page;
+		let ranking = 1;
 
-		const money = await Money.findAll({ where: { userID: { $ne: 'SLOTMACHINE' } }, order: 'money DESC' });
-
-		const paginated = util.paginate(money, page, Math.floor(config.paginationItems));
+		const money = await this.findCached();
+		const paginated = util.paginate(JSON.parse(money), page, Math.floor(config.paginationItems));
 
 		const embed = {
 			color: 3447003,
 			description: stripIndents`
 				__**Donut leaderboard, page ${paginated.page}**__
 
-				${paginated.items.map(user => `**-** ${`${this.client.users.get(user.userID).username}#${this.client.users.get(user.userID).discriminator}`} (**${user.money}** 🍩)`).join('\n')}
+				${paginated.items.map(user => `**${ranking++} -** ${`${this.client.users.get(user.userID).username}#${this.client.users.get(user.userID).discriminator}`} (**${user.money}** 🍩)`).join('\n')}
 				${paginated.maxPage > 1 ? `\nUse \`donut-leaderboard <page>\` to view a specific page.\n` : ''}
 			`
 		};
 
 		return msg.embed(embed);
+	}
+
+	async findCached() {
+		return redis.db.getAsync('moneyleaderboard').then(async reply => {
+			if (reply) {
+				return reply;
+			} else {
+				const money = await Money.findAll({ where: { userID: { $ne: 'SLOTMACHINE' } }, order: 'money DESC' });
+				if (!money) return `No money biatch`;
+
+				redis.db.setAsync('moneyleaderboard', JSON.stringify(money));
+				redis.db.expire('moneyleaderboard', 1800);
+
+				return JSON.stringify(money);
+			}
+		});
 	}
 };
