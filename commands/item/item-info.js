c732cc3dcf47bc2c61c32d0e19aba98f09cb5e71
Crawl <icrawltogo@gmail.com>
2017-01-02 15:19:28 +0900

    Fix storeItem name

--- commands/item/item-info.js
+++ commands/item/item-info.js
@@ -25,9 +25,10 @@ module.exports = class StoreInfoCommand extends Command {
 	async run(msg, args) {
 		const item = args.item.toLowerCase();
 		const storeItem = Store.getItem(item);
-		const storeItemName = storeItem.replace(/(\b\w)/gi, lc => lc.toUpperCase());
 
 		if (!storeItem) return msg.reply(`sorry, but that item doesn't exist. You can use ${this.client.commandPrefix}store-items to get a list of the available items.`);
+
+		const storeItemName = storeItem.name.replace(/(\b\w)/gi, lc => lc.toUpperCase());
 
 		return msg.reply(`one ${storeItemName} costs ${storeItem.price} 🍩s`);
 	}
