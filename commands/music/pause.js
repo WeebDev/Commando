c90dc621fa1a2e8a0f7b6a704883c8b0dde01ceb
Crawl <icrawltogo@gmail.com>
2017-01-07 17:02:27 +0100

    Some more little things

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -19,7 +19,7 @@ module.exports = class PauseSongCommand extends Command {
 	async run(msg) {
 		const queue = this.queue.get(msg.guild.id);
 		if (!queue) return msg.reply(`there isn't any music playing to pause, oh brilliant one.`);
-		if (!queue.songs[0].dispatcher) return msg.reply('it\'s kind of tough to pause a song that hasn\'t even begun playing yet.');
+		if (!queue.songs[0].dispatcher) return msg.reply('I can\'t pause a song that hasn\'t even begun playing yet.');
 		if (!queue.songs[0].playing) return msg.reply('pausing a song that is already paused is a bad move.');
 		queue.songs[0].dispatcher.pause();
 		queue.songs[0].playing = false;
