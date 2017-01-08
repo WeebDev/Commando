99a269567d3cdd768ea94e02c88b87f21cf278b1
TeeSeak <alex.theoddor@gmail.com>
2017-01-09 00:18:47 +0200

    Make var for await . -.

--- commands/games/tictactoe.js
+++ commands/games/tictactoe.js
@@ -68,7 +68,8 @@ module.exports = class TicTacToeCommand extends Command {
 			return msg.reply(`you can't start 2 games of Tic-Tac-Toe on the same server at once.`);
 		}
 
-		if (await this.confirmed(user, bet, msg)) return msg.reply(`your challenge has not been accepted.`);
+		const confirmed = this.confrimed(user, bet, msg);
+		if (!confirmed) return msg.reply(`your challenge has not been accepted.`);
 
 		games.push(msg.guild.id);
 
@@ -109,9 +110,9 @@ module.exports = class TicTacToeCommand extends Command {
 				time: 30e3
 			});
 
-			if (responses.size === 0) return resolve(true);
-			if (responses.first().content.toLowerCase() === 'reject') return resolve(true);
-			return resolve(false);
+			if (responses.size === 0) return resolve(false);
+			if (responses.first().content.toLowerCase() === 'reject') return resolve(false);
+			return resolve(true);
 		});
 	}
 
