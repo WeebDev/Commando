5fc313efde1f8f7ac21459f220a7dcc9dc99bd18
Alex <alex.theoddor@gmail.com>
2017-01-10 00:04:36 +0200

    Add daily donations (#29)
    * Add daily donations
    
    * Remove extra line break
    
    * Fix string
    
    * Make values class proprieties
    
    * *Facepalm*

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -16,24 +16,39 @@ module.exports = class DailyCommand extends Command {
 			throttling: {
 				usages: 2,
 				duration: 3
-			}
+			},
+
+			args: [
+				{
+					key: 'member',
+					prompt: 'to which user would you like to give your daily?\n',
+					type: 'member',
+					default: ''
+				}
+			]
 		});
 	}
 
-	async run(msg) {
+	async run(msg, args) {
 		const received = await Daily.received(msg.author.id);
+		const user = args.member;
 
 		if (received) {
 			const nextDaily = await Daily.nextDaily(msg.author.id);
 			return msg.reply(stripIndents`
-				you have already received your daily donuts.
-				You can receive your next daily in ${moment.duration(nextDaily).format('hh [hours] mm [minutes]')}
+				you have already received/given your daily donuts.
+				You can receive/give your next daily in ${moment.duration(nextDaily).format('hh [hours] mm [minutes]')}
 			`);
+		}
+
+		if (user) {
+			Daily.receive(msg.author.id, user.id);
+
+			return msg.reply(`${user} has successfully received your daily ${Daily.donationPayout} 🍩s.`);
 		}
 
 		Daily.receive(msg.author.id);
 
-		return msg.reply(`You have successfully received your daily ${Daily.dailyDonuts} 🍩s.`);
+		return msg.reply(`You have successfully received your daily ${Daily.normalPayout} 🍩s.`);
 	}
 };
-
--- currency/Daily.js
+++ currency/Daily.js
@@ -6,8 +6,12 @@ const redis = new Redis();
 const dayInMS = 24 * 60 * 60 * 1000;
 
 module.exports = class Daily {
-	static get dailyDonuts() {
+	static get normalPayout() {
 		return 210;
+	}
+
+	static get donationPayout() {
+		return 300;
 	}
 
 	static async received(userID) {
@@ -24,8 +28,10 @@ module.exports = class Daily {
 		return dayInMS - (Date.now() - lastDaily);
 	}
 
-	static receive(userID) {
-		Currency.addBalance(userID, Daily.dailyDonuts);
+	static receive(userID, donationID) {
+		if (donationID) Currency.addBalance(donationID, Daily.donationPayout);
+		else Currency.addBalance(userID, Daily.normalPayout);
+
 		redis.db.setAsync(`daily${userID}`, Date.now());
 		redis.db.expire(`daily${userID}`, dayInMS / 1000);
 	}
