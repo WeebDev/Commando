761465010234af7a191d37d525cf541df3881cd3
vzwGrey <thegreylp@googlemail.com>
2017-01-09 17:13:43 +0100

    Add user blacklisting

---
+++ commands/util/blacklist-user.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class BlacklistUserCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'blacklist-user',
+			aliases: ['blacklist'],
+			group: 'util',
+			memberName: 'blacklist-user',
+			description: 'Prohibit a user from using commando',
+
+			args: [
+				{
+					key: 'user',
+					prompt: 'What user should get blacklisted?',
+					type: 'user'
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		return this.client.options.owner === msg.author.id;
+	}
+
+	async run(msg, args) {
+		const user = args.user;
+
+		if (this.client.options.owner === user.id) {
+			return msg.reply('the bot owner can not be blacklisted.');
+		}
+
+		const blacklist = this.client.provider.get('global', 'userBlacklist', []);
+
+		if (blacklist.includes(user.id)) {
+			return msg.reply('that user is already blacklisted.');
+		}
+
+		blacklist.push(user.id);
+
+		this.client.provider.set('global', 'userBlacklist', blacklist);
+
+		return msg.reply(`${user.username}#${user.discriminator} has been blacklisted from using ${this.client.user.username}.`);
+	}
+};
---
+++ commands/util/whitelist-user.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class WhitelistUserCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'whitelist-user',
+			aliases: ['whitelist'],
+			group: 'util',
+			memberName: 'whitelist-user',
+			description: 'Remove a user from the blacklist',
+
+			args: [
+				{
+					key: 'user',
+					prompt: 'What user should get removed from the blacklist?',
+					type: 'user'
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		return this.client.options.owner === msg.author.id;
+	}
+
+	async run(msg, args) {
+		const user = args.user;
+
+		const blacklist = this.client.provider.get('global', 'userBlacklist', []);
+
+		if (!blacklist.includes(user.id)) return msg.reply('that user is not blacklisted.');
+
+		const index = blacklist.indexOf(user.id);
+		blacklist.splice(index, 1);
+
+		if (blacklist.length === 0) {
+			this.client.provider.remove('global', 'userBlacklist');
+		}
+		else {
+			this.client.provider.set('global', 'userBlacklist', blacklist);
+		}
+
+		return msg.reply(`${user.username}#${user.discriminator} has been removed from the blacklist.`);
+	}
+};
