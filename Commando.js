340c7dcb3d6f96de9b2b6a2289b55fa5421cba44
Kye Norman-Gill <kye.normangill@tafensw.net.au>
2017-02-28 21:09:45 +1100

    Discord invite links will be deleted

--- Commando.js
+++ Commando.js
@@ -59,6 +59,10 @@ client.on('error', winston.error)
 	.on('message', async (message) => {
 		if (message.channel.type === 'dm') return;
 
+		if (message.guild.id === '222078108977594368' && !message.member.roles.has('242700009961816065') && /https:\/\/discord\.gg\/\w+/.test(message.content)) {
+  			message.delete();
+  			message.reply('Please do not post invite links on this server. If you wish to give invite links, do so in direct messages.');
+  		}
 		const channelLocks = client.provider.get(message.guild.id, 'locks', []);
 		if (channelLocks.includes(message.channel.id)) return;
 		if (message.author.bot) return;
