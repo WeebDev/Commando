7fae80030c440737a621bdded4721e84017db72f
ItsJordan <ItsJordan@users.noreply.github.com>
2017-03-02 11:34:44 -0500

    Please merge <3

--- Commando.js
+++ Commando.js
@@ -63,6 +63,7 @@ client.on('error', winston.error)
 			if (message.deletable && message.author.id !== client.user.id) message.delete();
 			message.reply('Please do not post invite links on this server. If you wish to give invite links, do so in direct messages.');
 		}
+		if (message.guild.id === '222078108977594368' && !message.member.roles.has('242700009961816065') && /jordy|jordie|jorjor/.test(message.content) && message.deleteable)) message.delete()
 		const channelLocks = client.provider.get(message.guild.id, 'locks', []);
 		if (channelLocks.includes(message.channel.id)) return;
 		if (message.author.bot) return;
