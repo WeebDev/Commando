951d16f1bfa318756ec6de8c7f58237c180649be
TeeSeal <alex.theoddor@gmail.com>
2017-03-02 23:34:09 +0200

    Implement item trades

---
+++ commands/item/give.js
@@ -0,0 +1,73 @@
+const { Command } = require('discord.js-commando');
+
+const Inventory = require('../../currency/Inventory');
+const ItemGroup = require('../../currency/ItemGroup');
+
+module.exports = class ItemGiveCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'give',
+			aliases: [
+				'give-item',
+				'give-itmes',
+				'item-give',
+				'items-give'
+			],
+			group: 'item',
+			memberName: 'give',
+			description: `Give your items to another user.`,
+			guildOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 3
+			},
+
+			args: [
+				{
+					key: 'member',
+					prompt: 'what user would you like to give your item(s)?\n',
+					type: 'member'
+				},
+				{
+					key: 'amount',
+					label: 'amount of items to give',
+					prompt: 'how many items do you want to give to that user?\n',
+					validate: amount => {
+						return /^(?:\d+|-all)$/g.test(amount);
+					}
+				},
+				{
+					key: 'item',
+					prompt: 'what item would you like to give?\n',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const user = args.member;
+		const item = args.item.toLowerCase();
+		const inventory = await Inventory.fetchInventory(msg.author.id);
+
+		if (!inventory.content[item]) return msg.reply(`you don't have any ${item}s.`);
+
+		const itemBalance = inventory.content[item].amount;
+		const amount = args.amount === '-all'
+			? inventory.content[item].amount
+			: parseInt(args.amount);
+
+		if (user.id === msg.author.id) return msg.reply("giving items to yourself won't change anything.");
+		if (user.user.bot) return msg.reply("don't give your items to bots: they're bots, man.");
+		if (amount <= 0) return msg.reply("you can't trade 0 or less items.");
+		if (amount > itemBalance) return msg.reply(`you only have ${itemBalance} ${item}(s).`);
+
+		const itemGroup = new ItemGroup(item, amount);
+		const receiveInv = await Inventory.fetchInventory(user.id);
+
+		inventory.removeItems(itemGroup);
+		receiveInv.addItems(itemGroup);
+
+		return msg.reply(`${user.displayName} successfully received your item(s)!`);
+	}
+};
---
+++ commands/item/trade.js
@@ -0,0 +1,143 @@
+const { Command } = require('discord.js-commando');
+
+const Currency = require('../../currency/Currency');
+const Inventory = require('../../currency/Inventory');
+const ItemGroup = require('../../currency/ItemGroup');
+
+module.exports = class ItemTradeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'item-trade',
+			aliases: [
+				'trade-items',
+				'trade-item',
+				'items-trade'
+			],
+			group: 'item',
+			memberName: 'trade',
+			description: `Trade items with another user.`,
+			guildOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 3
+			},
+
+			args: [
+				{
+					key: 'member',
+					prompt: 'what user would you like to give your item(s)?\n',
+					type: 'member'
+				},
+				{
+					key: 'offerAmount',
+					label: 'amount of items to give',
+					prompt: 'how many would you like to offer?\n',
+					type: 'integer',
+					min: 1
+				},
+				{
+					key: 'offerItem',
+					prompt: 'what item would you like to offer?\n',
+					type: 'string'
+				},
+				{
+					key: 'receiveAmount',
+					label: 'amount of items to receive',
+					prompt: 'how many would you like to receive?\n',
+					type: 'integer',
+					min: 1
+				},
+				{
+					key: 'receiveItem',
+					prompt: 'what item would you like to receive?\n',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const user = args.member;
+		const offerAmount = args.offerAmount;
+		const receiveAmount = args.receiveAmount;
+
+		const offerItem = args.offerItem.toLowerCase().match(/^donuts?$/) ? '' : args.offerItem.toLowerCase();
+		const receiveItem = args.receiveItem.toLowerCase().match(/^donuts?$/) ? '' : args.receiveItem.toLowerCase();
+
+		if (user.id === msg.author.id) return msg.reply('what are you trying to achieve by trading with yourself?');
+		if (user.user.bot) return msg.reply('bots got nothing to trade, man.');
+		if (!(offerItem + receiveItem)) return msg.reply("you can't trade donuts for donuts.");
+
+		const offerBalance = Currency.getBalance(msg.author.id);
+		const receiveBalance = Currency.getBalance(user.id);
+
+		const offerInv = Inventory.fetchInventory(msg.author.id);
+		const receiveInv = Inventory.fetchInventory(user.id);
+
+		const offerItemBalance = offerInv.content[offerItem] ? offerInv.content[offerItem].amount : 0;
+		const receiveItemBalance = receiveInv.content[receiveItem] ? receiveInv.content[receiveItem].amount : 0;
+
+		if (!offerItem && offerAmount > offerBalance) return msg.reply(`you only have ${Currency.convert(offerBalance)}`);
+		if (!receiveItem && receiveAmount > receiveBalance) return msg.reply(`${user.displayName} only has ${Currency.convert(receiveBalance)}`);
+		if (offerAmount > offerItemBalance) return msg.reply(`,you have ${offerItemBalance} ${offerItem}s.`);
+		if (receiveAmount > receiveItemBalance) return msg.reply(`,${user.displayName} has ${receiveItemBalance} ${receiveItem}s.`);
+
+		const embed = {
+			title: `${msg.member.displayName} < -- > ${user.displayName}`,
+			description: 'Type `accept` within the next 30 seconds to accept this offer.',
+			fields: [
+				{
+					name: msg.member.displayName,
+					value: offerItem
+						? `${offerAmount} ${offerItem}`
+						: Currency.convert(offerAmount)
+				},
+				{
+					name: user.displayName,
+					value: receiveItem
+						? `${receiveAmount} ${receiveItem}`
+						: Currency.convert(receiveAmount)
+				}
+			]
+		};
+
+		if (!await this.response(msg, user, embed)) return msg.reply(`${user.displayName} declined or failed to respond.`);
+
+		if (!offerItem) this.sendDonuts(msg.author, user, offerAmount);
+		else this.sendItems(offerInv, receiveInv, offerItem, offerAmount);
+
+		if (!receiveItem) this.sendDonuts(user, msg.author, receiveAmount);
+		else this.sendItems(receiveInv, offerInv, receiveItem, receiveAmount);
+
+		return msg.say('Trade successful.');
+	}
+
+	sendItems(fromInventory, toInventory, item, amount) {
+		const itemGroup = new ItemGroup(item, amount);
+
+		fromInventory.removeItems(itemGroup);
+		toInventory.addItems(itemGroup);
+	}
+
+	sendDonuts(fromUser, toUser, amount) {
+		Currency.removeBalance(fromUser, amount);
+		Currency.addBalance(toUser, amount);
+	}
+
+	response(msg, user, embed) {
+		return new Promise(async resolve => {
+			msg.say(`${user}, ${msg.member.displayName} wants to trade with you.`);
+			msg.embed(embed);
+
+			const responses = await msg.channel.awaitMessages(response => {
+				return response.author.id === user.id && response.content.toLowerCase() === 'accept';
+			}, {
+				maxMatches: 1,
+				time: 30e3
+			});
+
+			if (responses.size === 0) resolve(false);
+			resolve(true);
+		});
+	}
+};
