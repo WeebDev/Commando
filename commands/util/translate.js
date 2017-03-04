5554781dccba3179ef2ef090fb684ec90d2bda79
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-05 03:58:23 +1100

    Add ?translate

---
+++ commands/util/translate.js
@@ -0,0 +1,78 @@
+const { Command } = require('discord.js-commando');
+const request = require('request-promise');
+
+const { version } = require('../../package');
+
+module.exports = class TranslateCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'translate',
+			aliases: ['t'],
+			group: 'translate',
+			memberName: 'translate',
+			description: 'Translates text.',
+			throttling: {
+				usages: 5,
+				duration: 60
+			},
+
+			args: [
+				{
+					key: 'query',
+					prompt: 'what text do you want to translate?\n',
+					type: 'string'
+				},
+				{
+					key: 'to',
+					prompt: 'what languages do you want to translate to?\n',
+					type: 'string'
+				},
+				{
+					key: 'from',
+					prompt: 'what languages do you want to translate from?\n',
+					type: 'string',
+					default: ''
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const response = await request({
+			uri: `https://api.kurisubrooks.com/api/translate?to=${args.to}&from=${args.from}&query=${encodeURIComponent(args.query)}`,
+			headers: { 'User-Agent': `Commando v${version} (https://github.com/WeebDev/Commando/)` },
+			json: true
+		});
+
+		if (!response.ok) return msg.reply(this.handleError(response));
+
+		const embed = {
+			author: {
+				name: msg.member ? msg.member.displayName : msg.author.username,
+				icon_url: msg.author.avatarURL // eslint-disable-line camelcase
+			},
+			fields: [
+				{
+					name: response.from.name,
+					value: response.query
+				},
+				{
+					name: response.to.name,
+					value: response.result
+				}
+			]
+		};
+
+		return msg.embed(embed);
+	}
+
+	handleError(response) {
+		if (response.error === `unknown language in 'to' field`) {
+			return 'Unknown \'to\' Language';
+		} else if (response.error === `unknown language in 'from' field`) {
+			return 'Unknown \'from\' Language';
+		} else {
+			return 'Internal Server Error';
+		}
+	}
+};
