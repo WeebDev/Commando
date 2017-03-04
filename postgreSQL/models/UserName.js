fd268317eda6afee5cd350b349391f28aed25091
Kye Norman-Gill <kye.normangill@tafensw.net.au>
2017-03-05 04:48:18 +1100

    add composite key for duplicate usernames

--- postgreSQL/models/UserName.js
+++ postgreSQL/models/UserName.js
@@ -7,6 +7,11 @@ const database = new Database();
 const UserName = database.db.define('UserNames', {
 	userid: Sequelize.STRING,
 	username: Sequelize.STRING
+}, {
+	indexes: [{
+		fields: ['userid', 'username'],
+		unique: true
+	}]
 });
 
 UserName.sync();
