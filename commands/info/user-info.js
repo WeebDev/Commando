6fdc1b84dfd3943f8e73f4a414c3f00fbb4ad69b
Kye Norman-Gill <kye.normangill@tafensw.net.au>
2017-03-05 07:50:15 +1100

    Used await cuz es7 :lul:

--- commands/info/user-info.js
+++ commands/info/user-info.js
@@ -32,32 +32,32 @@ module.exports = class UserInfoCommand extends Command {
 		const member = args.member;
 		const user = member.user;
 
-		userName.findAll({where: {userid: user.id}}).then((userName) => {
-			return msg.embed({
-				color: 3447003,
-				fields: [
-					{
-						name: '❯ Member Details',
-						value: stripIndents`
-							${member.nickname !== null ? ` • Nickname: ${member.nickname}` : '• No nickname'}
-							• Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(' ')}
-							• Joined at: ${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}
-						`
-					},
-					{
-						name: '❯ User Details',
-						value: stripIndents`
-							• Created at: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}${user.bot
-								? '\n• Is a bot account'
-								: ''}
-							• Aliases: ${userName.length ? userName.map(u => u.username).join(", ") : user.username}
-							• Status: ${user.presence.status}
-							• Game: ${user.presence.game ? user.presence.game.name : 'None'}
-						`
-					}
-				],
-				thumbnail: { url: user.avatarURL }
-			});
+		const userNames = await userName.findAll({where: {userid: user.id}});
+
+		return msg.embed({
+			color: 3447003,
+			fields: [
+				{
+					name: '❯ Member Details',
+					value: stripIndents`
+						${member.nickname !== null ? ` • Nickname: ${member.nickname}` : '• No nickname'}
+						• Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(' ')}
+						• Joined at: ${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}
+					`
+				},
+				{
+					name: '❯ User Details',
+					value: stripIndents`
+						• Created at: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}${user.bot
+							? '\n• Is a bot account'
+							: ''}
+						• Aliases: ${userNames.length ? userNames.map(u => u.username).join(", ") : user.username}
+						• Status: ${user.presence.status}
+						• Game: ${user.presence.game ? user.presence.game.name : 'None'}
+					`
+				}
+			],
+			thumbnail: { url: user.avatarURL }
 		});
 	}
 };
