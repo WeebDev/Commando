ee70d744f34c0929ab9ae8cf1d61e2ebd6df42ce
Crawl <icrawltogo@gmail.com>
2017-03-28 15:35:25 +0200

    Update please.js

--- commands/social/please.js
+++ commands/social/please.js
@@ -30,7 +30,7 @@ module.exports = class PleaseCommand extends Command {
 		const member = args.member.displayName || 'Grey';
 		const canvas = new Canvas();
 		const ctx = canvas.getContext('2d');
-		const { width, height } = this.textSizes(ctx, user);
+		const { width, height } = this.textSizes(ctx, member);
 
 		canvas.width = width;
 		canvas.height = height;
