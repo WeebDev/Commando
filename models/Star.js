35d366f2a9128d8ebb95f6a2d2168382781aef2d
vzwGrey <thegreylp@googlemail.com>
2017-04-21 00:19:20 +0200

    Increase Star content size limit

--- models/Star.js
+++ models/Star.js
@@ -10,7 +10,7 @@ const Star = Database.db.define('stars', {
 		unique: true
 	},
 	content: {
-		type: Sequelize.STRING,
+		type: Sequelize.STRING(2000), // eslint-disable-line new-cap
 		allowNull: false
 	},
 	authorID: {
