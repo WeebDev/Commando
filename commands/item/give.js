a9d25e11f0768d6777571765855e3ecf5e29d0d2
vzwGrey <thegreylp@googlemail.com>
2017-04-27 12:18:11 +0200

    Fix item trading

--- commands/item/give.js
+++ commands/item/give.js
@@ -40,21 +40,21 @@ module.exports = class ItemGiveCommand extends Command {
 	}
 
 	async run(msg, args) {
-		const { user, amount } = args;
+		const { member, amount } = args;
 		const item = ItemGroup.convert(args.item, amount);
 		const inventory = await Inventory.fetchInventory(msg.author.id);
 		const itemBalance = inventory.content[item] ? inventory.content[item].amount : 0;
 
-		if (user.id === msg.author.id) return msg.reply("giving items to yourself won't change anything.");
-		if (user.user.bot) return msg.reply("don't give your items to bots: they're bots, man.");
+		if (member.id === msg.author.id) return msg.reply("giving items to yourself won't change anything.");
+		if (member.user.bot) return msg.reply("don't give your items to bots: they're bots, man.");
 		if (amount <= 0) return msg.reply("you can't give 0 or less items.");
 		if (amount > itemBalance) return msg.reply(`you have ${itemBalance} ${item}(s).`);
 
 		const itemGroup = new ItemGroup(item, amount);
-		const receiveInv = await Inventory.fetchInventory(user.id);
+		const receiveInv = await Inventory.fetchInventory(member.id);
 		inventory.removeItems(itemGroup);
 		receiveInv.addItems(itemGroup);
 
-		return msg.reply(`${user.displayName} successfully received your item(s)!`);
+		return msg.reply(`${member.displayName} successfully received your item(s)!`);
 	}
 };
