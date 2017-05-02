9aeec9f570136a26241d425f0fbefab5b718cbc5
Crawl <icrawltogo@gmail.com>
2017-05-02 03:54:18 +0200

    Add deploy.sh

---
+++ deploy/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+function test {
+	npm test
+}
+
+if [ "$TRAVIS_BRANCH" == revert-* ]; then
+	echo -e "\e[36m\e[1mBuild triggered for reversion branch \"${TRAVIS_BRANCH}\" - running nothing."
+	exit 0
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	echo -e "\e[36m\e[1mBuild triggered for PR #${TRAVIS_PULL_REQUEST} to branch \"${TRAVIS_BRANCH}\" - only running test."
+	test
+	exit 0
+fi
+
+if [ -n "$TRAVIS_TAG" ]; then
+	echo -e "\e[36m\e[1mBuild triggered for tag \"${TRAVIS_TAG}\"."
+	DOCKER_RELEASE=$TRAVIS_TAG
+	test
+	docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
+	docker build -t tohru .
+	docker tag tohru:latest crawl/tohru:$DOCKER_RELEASE
+	docker push crawl/tohru:$DOCKER_RELEASE
+else
+	echo -e "\e[36m\e[1mBuild triggered for branch \"${TRAVIS_BRANCH}\"."
+	DOCKER_RELEASE="latest"
+	test
+	docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
+	docker build -t tohru .
+	docker tag tohru:latest crawl/tohru:$DOCKER_RELEASE
+	docker push crawl/tohru:$DOCKER_RELEASE
+fi
