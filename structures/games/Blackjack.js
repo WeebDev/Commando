04d87072ffb160a2ad5f439877d7a93a3a7ca6be
Crawl <icrawltogo@gmail.com>
2017-05-16 01:56:42 +0200

    Make sure to create a new array

--- structures/games/Blackjack.js
+++ structures/games/Blackjack.js
@@ -94,14 +94,15 @@ class Blackjack {
 	}
 
 	static _shuffle(array) {
+		const newArray = array.slice();
 		for (let i = array.length - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
-			const temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			const temp = newArray[i];
+			newArray[i] = newArray[j];
+			newArray[j] = temp;
 		}
 
-		return array;
+		return newArray;
 	}
 }
 
