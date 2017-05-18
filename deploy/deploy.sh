60cf0e154e8cbdcfc1e3beefa4af40fa80e84813
TBK <tbk@jjtc.dk>
2017-05-19 00:50:37 +0200

    ShellChecke
    http://www.shellcheck.net/

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,7 @@ function test {
 	npm test
 }
 
-if [ "$TRAVIS_BRANCH" == revert-* ]; then
+if [[ "$TRAVIS_BRANCH" == revert-* ]]; then
 	echo -e "\e[36m\e[1mBuild triggered for reversion branch \"${TRAVIS_BRANCH}\" - running nothing."
 	exit 0
 fi
@@ -19,18 +19,18 @@ fi
 
 if [ -n "$TRAVIS_TAG" ]; then
 	echo -e "\e[36m\e[1mBuild triggered for tag \"${TRAVIS_TAG}\"."
-	DOCKER_RELEASE=$TRAVIS_TAG
+	DOCKER_RELEASE="$TRAVIS_TAG"
 	test
 	docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 	docker build -t commando .
-	docker tag commando:latest crawl/commando:$DOCKER_RELEASE
-	docker push crawl/commando:$DOCKER_RELEASE
+	docker tag commando:latest crawl/commando:"$DOCKER_RELEASE"
+	docker push crawl/commando:"$DOCKER_RELEASE"
 else
 	echo -e "\e[36m\e[1mBuild triggered for branch \"${TRAVIS_BRANCH}\"."
 	DOCKER_RELEASE="latest"
 	test
 	docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 	docker build -t commando .
-	docker tag commando:latest crawl/commando:$DOCKER_RELEASE
-	docker push crawl/commando:$DOCKER_RELEASE
+	docker tag commando:latest crawl/commando:"$DOCKER_RELEASE"
+	docker push crawl/commando:"$DOCKER_RELEASE"
 fi
