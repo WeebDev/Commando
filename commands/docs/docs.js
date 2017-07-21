051f40d31118142f1b1d7aa31e1e7c2fa5ee6a2d
1Computer1 <mineox100@gmail.com>
2017-07-20 23:16:19 -0400

    Improve docs command

--- commands/docs/docs.js
+++ commands/docs/docs.js
@@ -53,11 +53,11 @@ module.exports = class DocsCommand extends Command {
 
 		const findWithin = (parentItem, props, name) => {
 			let found = null;
-			for (const type of props) {
-				if (!parentItem[type]) continue;
-				const item = parentItem[type].find(i => i.name.toLowerCase() === name);
+			for (const category of props) {
+				if (!parentItem[category]) continue;
+				const item = parentItem[category].find(i => i.name.toLowerCase() === name);
 				if (item) {
-					found = { item, type };
+					found = { item, category };
 					break;
 				}
 			}
@@ -75,9 +75,16 @@ module.exports = class DocsCommand extends Command {
 			classes: ['props', 'methods', 'events'],
 			interfaces: ['props', 'methods', 'events'],
 			typedefs: ['props']
-		}[main.type], memberQuery);
+		}[main.category], memberQuery);
 
 		if (!member) return [];
+		const rest = query.slice(2);
+
+		if (rest.length) {
+			const base = this.joinType(member.item.type).replace(/<.+?>/g, '');
+			return this.search(docs, `${base}.${rest.join('.')}`);
+		}
+
 		res.push(member);
 		return res;
 	}
@@ -92,62 +99,69 @@ module.exports = class DocsCommand extends Command {
 		return type.map(t => t.map(a => Array.isArray(a) ? a.join('') : a).join('')).join(' | ');
 	}
 
-	makeLink(mainItem, item, version) {
+	makeLink(main, member, version) {
 		return oneLineTrim`
-			https://discord.js.org/#/docs/main/${version}/class/${mainItem.name}
-			?scrollTo=${item.scope === 'static' ? 's-' : ''}${item.name}
+			https://discord.js.org/#/docs/main/${version}/
+			${main.category === 'classes' ? 'class' : 'typedef'}/${main.item.name}
+			?scrollTo=${member.item.scope === 'static' ? 's-' : ''}${member.item.name}
 		`;
 	}
 
-	formatMain(item, version) {
+	formatMain(main, version) {
 		const embed = {
-			description: `__**[${item.name}`,
+			description: `__**[${main.item.name}`,
 			fields: []
 		};
 
-		if (item.extends) embed.description += ` (extends ${item.extends[0]})`;
-		embed.description += `](https://discord.js.org/#/docs/main/${version}/class/${item.name})**__\n`;
-		if (item.description) embed.description += `\n${this.clean(item.description)}`;
+		if (main.item.extends) embed.description += ` (extends ${main.item.extends[0]})`;
+
+		embed.description += oneLineTrim`
+			](https://discord.js.org/#/docs/main/${version}/
+			${main.category === 'classes' ? 'class' : 'typedef'}/${main.item.name})**__
+		`;
+		embed.description += '\n';
+
+		if (main.item.description) embed.description += `\n${this.clean(main.item.description)}`;
 
 		const join = it => `\`${it.map(i => i.name).join('` `')}\``;
 
-		if (item.props) {
+		if (main.item.props) {
 			embed.fields.push({
 				name: 'Properties',
-				value: join(item.props)
+				value: join(main.item.props)
 			});
 		}
 
-		if (item.methods) {
+		if (main.item.methods) {
 			embed.fields.push({
 				name: 'Methods',
-				value: join(item.methods)
+				value: join(main.item.methods)
 			});
 		}
 
-		if (item.events) {
+		if (main.item.events) {
 			embed.fields.push({
 				name: 'Events',
-				value: join(item.events)
+				value: join(main.item.events)
 			});
 		}
 
 		return embed;
 	}
 
-	formatProp(item, mainItem, version) {
+	formatProp(main, member, version) {
 		const embed = {
 			description: oneLineTrim`
-				__**[${mainItem.name}${item.scope === 'static' ? '.' : '#'}${item.name}]
-				(${this.makeLink(mainItem, item, version)})**__
+				__**[${main.item.name}${member.item.scope === 'static' ? '.' : '#'}${main.item.name}]
+				(${this.makeLink(main, member, version)})**__
 			`,
 			fields: []
 		};
 
 		embed.description += '\n';
-		if (item.description) embed.description += `\n${this.clean(item.description)}`;
+		if (member.item.description) embed.description += `\n${this.clean(member.item.description)}`;
 
-		const type = this.joinType(item.type);
+		const type = this.joinType(member.item.type);
 		embed.fields.push({
 			name: 'Type',
 			value: `\`${type}\``
@@ -156,20 +170,20 @@ module.exports = class DocsCommand extends Command {
 		return embed;
 	}
 
-	formatMethod(item, mainItem, version) {
+	formatMethod(main, member, version) {
 		const embed = {
 			description: oneLineTrim`
-				__**[${mainItem.name}${item.scope === 'static' ? '.' : '#'}${item.name}()]
-				(${this.makeLink(mainItem, item, version)})**__
+				__**[${main.item.name}${member.item.scope === 'static' ? '.' : '#'}${member.item.name}()]
+				(${this.makeLink(main, member, version)})**__
 			`,
 			fields: []
 		};
 
 		embed.description += '\n';
-		if (item.description) embed.description += `\n${this.clean(item.description)}`;
+		if (member.item.description) embed.description += `\n${this.clean(member.item.description)}`;
 
-		if (item.params) {
-			const params = item.params.map(param => {
+		if (member.item.params) {
+			const params = member.item.params.map(param => {
 				const name = param.optional ? `[${param.name}]` : param.name;
 				const type = this.joinType(param.type);
 				return `\`${name}: ${type}\`\n${this.clean(param.description)}`;
@@ -181,9 +195,9 @@ module.exports = class DocsCommand extends Command {
 			});
 		}
 
-		if (item.returns) {
-			const desc = item.returns.description ? `${this.clean(item.returns.description)}\n` : '';
-			const type = this.joinType(item.returns.types || item.returns);
+		if (member.item.returns) {
+			const desc = member.item.returns.description ? `${this.clean(member.item.returns.description)}\n` : '';
+			const type = this.joinType(member.item.returns.types || member.item.returns);
 			const returns = `${desc}\`=> ${type}\``;
 			embed.fields.push({
 				name: 'Returns',
@@ -199,16 +213,16 @@ module.exports = class DocsCommand extends Command {
 		return embed;
 	}
 
-	formatEvent(item, mainItem, version) {
+	formatEvent(main, member, version) {
 		const embed = {
-			description: `__**[${mainItem.name}#${item.name}](${this.makeLink(mainItem, item, version)})**__\n`,
+			description: `__**[${main.item.name}#${main.item.name}](${this.makeLink(main, member, version)})**__\n`,
 			fields: []
 		};
 
-		if (item.description) embed.description += `\n${this.clean(item.description)}`;
+		if (main.item.description) embed.description += `\n${this.clean(main.item.description)}`;
 
-		if (item.params) {
-			const params = item.params.map(param => {
+		if (main.item.params) {
+			const params = main.item.params.map(param => {
 				const type = this.joinType(param.type);
 				return `\`${param.name}: ${type}\`\n${this.clean(param.description)}`;
 			});
@@ -234,7 +248,7 @@ module.exports = class DocsCommand extends Command {
 			props: this.formatProp,
 			methods: this.formatMethod,
 			events: this.formatEvent
-		}[member.type].call(this, member.item, main.item, version) : this.formatMain(main.item, version);
+		}[member.category].call(this, main, member, version) : this.formatMain(main, version);
 
 		const icon = 'https://cdn.discordapp.com/icons/222078108977594368/bc226f09db83b9176c64d923ff37010b.webp';
 		embed.url = `https://discord.js.org/#/docs/main/${version}`;
